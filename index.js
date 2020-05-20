649c450efba68f23b3d9461f536d498b035dbe99
FichoZee <filip.zafran@gmail.com>
2020-05-20 17:58:15 +0200

    add toggle content to all 4 buttons

--- index.js
+++ index.js
@@ -1,21 +1,48 @@
-var tabButtons = document.querySelectorAll(".tabCont .buttonContainer button");
-var tabPanels = document.querySelectorAll(".tabCont .tabPanel1");
+  const cupDetailsBttn = document.getElementById("cupDetailsBttn");
+  cupDetailsBttn.addEventListener("click", toggleSideBar1);
 
-function showPanel(panelIndex, colorCode) {
-  tabButtons.forEach(function(node) {
-    node.style.backgroundColor = "";
-    node.style.color = "";
-  });
-  tabButtons[panelIndex].style.backgroundColor = "colorCode";
-  tabButtons[panelIndex].style.color = "black";
-  buttonContainer.forEach(function(node) {
-    node.style.display = "none";
-  });
-  buttonContainer[panelIndex].style.display = "block";
-  buttonContainer[panelIndex].style.backgroundColor = "colorCode";
+function toggleSideBar1() {
+  var x = document.getElementById("cupDetails");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+} 
+
+
+  const paymentDetailsBttn = document.getElementById("paymentDetailsBttn");
+  paymentDetailsBttn.addEventListener("click", toggleSideBar2);
+
+function toggleSideBar2() {  
+  var x = document.getElementById("paymentDetails");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+} 
+
+  const locationBttn = document.getElementById("locationBttn");
+  locationBttn.addEventListener("click", toggleSideBar3);
+
+
+function toggleSideBar3() {  
+  var x = document.getElementById("location");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
 }
 
-showPanel(0, "blue");
+      const appFormBttn = document.getElementById("appFormBttn");
+    appFormBttn.addEventListener("click", toggleSideBar4);
 
-// explanation
-// https://www.youtube.com/watch?v=PzSxehu4G78
+  function toggleSideBar4() {    
+  var x = document.getElementById("appForm");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+    }}
